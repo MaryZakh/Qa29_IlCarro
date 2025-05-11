877c101debfd46f4e599bdd90306088dc0a31507
Maria Z <z0559882272@gmail.com>
2025-05-11 19:40:04 +0300

    added User obj

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import models.User;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -13,6 +14,22 @@ public class LoginTests extends TestBase {
             app.getHelperUser().logout();
         }
     }
+
+    @Test
+    public void loginSuccess1() {
+        User user = new User().setEmail("margo@gmail.com").setPassword("Mmar123456$");
+
+//        user.setEmail("margo@gmail.com");
+//        user.setPassword("Mmar123456$");
+
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm(user);
+        app.getHelperUser().submitLogin();
+        //Assert if element with text "Logged in success" is present
+        Assert.assertEquals(app.getHelperUser().getMessage(), "Logged in success");
+    }
+
+
 
     @Test
     public void loginSuccess() {
